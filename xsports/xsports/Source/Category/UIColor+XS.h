9083987c4d8f7e321d1ba2408970d474170c0ad8
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-10 23:35:00 -0800

    add icon

---
+++ xsports/xsports/Source/Category/UIColor+XS.h
@@ -0,0 +1,48 @@
+//
+//  UIColor+XS.h
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/10/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (XS)
+
++ (UIColor *)lightBambooColor;
++ (UIColor *)bambooColor;
++ (UIColor *)semiBambooColor;
++ (UIColor *)darkBambooColor;
+
++ (UIColor *)lightWaveColor;
++ (UIColor *)waveColor;
++ (UIColor *)semiWaveColor;
++ (UIColor *)darkWaveColor;
+
++ (UIColor *)lightPebbleColor;
++ (UIColor *)pebbleColor;
++ (UIColor *)semiPebbleColor;
++ (UIColor *)darkPebbleColor;
+
++ (UIColor *)lightTempuraColor;
++ (UIColor *)tempuraColor;
++ (UIColor *)semiTempuraColor;
++ (UIColor *)darkTempuraColor;
+
++ (UIColor *)lightCoralColor;
++ (UIColor *)coralColor;
++ (UIColor *)semiCoralColor;
++ (UIColor *)darkCoralColor;
+
++ (UIColor *)lightJadeColor;
++ (UIColor *)jadeColor;
++ (UIColor *)semiJadeColor;
++ (UIColor *)darkJadeColor;
+
++ (UIColor *)lightFujiColor;
++ (UIColor *)fujiColor;
++ (UIColor *)semiFujiColor;
++ (UIColor *)darkFujiColor;
+
+@end
---
+++ xsports/xsports/Source/Category/UIColor+XS.m
@@ -0,0 +1,132 @@
+//
+//  UIColor+XS.m
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/10/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import "UIColor+XS.h"
+
+@implementation UIColor (XS)
+
++ (UIColor *)lightBambooColor
+{
+    return [UIColor colorWithRed:171.0/255.0f green:213.0/255.0f blue:60.0/255.0f alpha:1];
+}
++ (UIColor *)bambooColor
+{
+    return [UIColor colorWithRed:141.0/255.0f green:181.0/255.0f blue:37.0/255.0f alpha:1];
+}
++ (UIColor *)semiBambooColor
+{
+    return [UIColor colorWithRed:105.0/255.0f green:143.0/255.0f blue:14.0/255.0f alpha:1];
+}
++ (UIColor *)darkBambooColor
+{
+    return [UIColor colorWithRed:83.0/255.0f green:114.0/255.0f blue:4.0/255.0f alpha:1];
+}
+
++ (UIColor *)lightWaveColor
+{
+    return [UIColor colorWithRed:118.0/255.0f green:191.0/255.0f blue:222.0/255.0f alpha:1];
+}
++ (UIColor *)waveColor
+{
+    return [UIColor colorWithRed:84.0/255.0f green:160.0/255.0f blue:191.0/255.0f alpha:1];
+}
++ (UIColor *)semiWaveColor
+{
+    return [UIColor colorWithRed:26.0/255.0f green:117.0/255.0f blue:161.0/255.0f alpha:1];
+}
++ (UIColor *)darkWaveColor
+{
+    return [UIColor colorWithRed:12.0/255.0f green:84.0/255.0f blue:121.0/255.0f alpha:1];
+}
+
++ (UIColor *)lightPebbleColor
+{
+    return [UIColor colorWithRed:178.0/255.0f green:174.0/255.0f blue:165.0/255.0f alpha:1];
+}
++ (UIColor *)pebbleColor
+{
+    return [UIColor colorWithRed:143.0/255.0f green:141.0/255.0f blue:135.0/255.0f alpha:1];
+}
++ (UIColor *)semiPebbleColor
+{
+    return [UIColor colorWithRed:95.0/255.0f green:91.0/255.0f blue:85.0/255.0f alpha:1];
+}
++ (UIColor *)darkPebbleColor
+{
+    return [UIColor colorWithRed:67.0/255.0f green:65.0/255.0f blue:63.0/255.0f alpha:1];
+}
+
++ (UIColor *)lightTempuraColor
+{
+    return [UIColor colorWithRed:239.0/255.0f green:168.0/255.0f blue:94.0/255.0f alpha:1];
+}
++ (UIColor *)tempuraColor
+{
+    return [UIColor colorWithRed:202.0/255.0f green:120.0/255.0f blue:40.0/255.0f alpha:1];
+}
++ (UIColor *)semiTempuraColor
+{
+    return [UIColor colorWithRed:158.0/255.0f green:85.0/255.0f blue:18.0/255.0f alpha:1];
+}
++ (UIColor *)darkTempuraColor
+{
+    return [UIColor colorWithRed:120.0/255.0f green:58.0/255.0f blue:2.0/255.0f alpha:1];
+}
+
++ (UIColor *)lightCoralColor
+{
+    return [UIColor colorWithRed:240.0/255.0f green:140.0/255.0f blue:136.0/255.0f alpha:1];
+}
++ (UIColor *)coralColor
+{
+    return [UIColor colorWithRed:222.0/255.0f green:97.0/255.0f blue:94.0/255.0f alpha:1];
+}
++ (UIColor *)semiCoralColor
+{
+    return [UIColor colorWithRed:179.0/255.0f green:45.0/255.0f blue:44.0/255.0f alpha:1];
+}
++ (UIColor *)darkCoralColor
+{
+    return [UIColor colorWithRed:147.0/255.0f green:19.0/255.0f blue:20.0/255.0f alpha:1];
+}
+
++ (UIColor *)lightJadeColor
+{
+    return [UIColor colorWithRed:135.0/255.0f green:198.0/255.0f blue:192.0/255.0f alpha:1];
+}
++ (UIColor *)jadeColor
+{
+    return [UIColor colorWithRed:97.0/255.0f green:168.0/255.0f blue:161.0/255.0f alpha:1];
+}
++ (UIColor *)semiJadeColor
+{
+    return [UIColor colorWithRed:49.0/255.0f green:106.0/255.0f blue:100.0/255.0f alpha:1];
+}
++ (UIColor *)darkJadeColor
+{
+    return [UIColor colorWithRed:35.0/255.0f green:87.0/255.0f blue:82.0/255.0f alpha:1];
+}
+
++ (UIColor *)lightFujiColor
+{
+    return [UIColor colorWithRed:174.0/255.0f green:164.0/255.0f blue:194.0/255.0f alpha:1];
+}
++ (UIColor *)fujiColor
+{
+    return [UIColor colorWithRed:118.0/255.0f green:105.0/255.0f blue:146.0/255.0f alpha:1];
+}
++ (UIColor *)semiFujiColor
+{
+    return [UIColor colorWithRed:77.0/255.0f green:63.0/255.0f blue:108.0/255.0f alpha:1];
+}
++ (UIColor *)darkFujiColor
+{
+    return [UIColor colorWithRed:60.0/255.0f green:49.0/255.0f blue:83.0/255.0f alpha:1];
+}
+
+@end
