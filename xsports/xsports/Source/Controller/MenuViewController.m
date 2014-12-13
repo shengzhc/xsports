2ab74332bcd69ccc00222169093bb557b3aee87d
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-12 21:04:06 -0800

    update

--- xsports/xsports/Source/Controller/MenuViewController.m
+++ xsports/xsports/Source/Controller/MenuViewController.m
@@ -19,17 +19,19 @@
 
 - (void)setupLabel
 {
-    self.nameLabel.font = [UIFont mediumEngFont];
+    self.nameLabel.font = [UIFont mediumEngFontWithSize:18];
     self.nameLabel.textColor = [UIColor whiteColor];
     self.nameLabel.text = @"Frank Rapacciuolo";
     self.locationLabel.font = [UIFont mediumEngFontWithSize:14.0];
-    self.locationLabel.textColor = [UIColor lightPebbleColor];
+    self.locationLabel.textColor = [UIColor semiPebbleColor];
     self.locationLabel.text = @"San Francisco";
 }
 
 - (void)setupImageView
 {
     self.profileImageView.layer.cornerRadius = self.profileImageView.bounds.size.width/2.0;
+    self.profileImageView.layer.borderWidth = 3.0;
+    self.profileImageView.layer.borderColor = [UIColor darkWaveColor].CGColor;
 }
 
 @end
