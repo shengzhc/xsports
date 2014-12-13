694e13f0b78233f165df05532743a1853289cd7d
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-13 11:48:35 -0800

    update

--- xsports/xsports/Source/Controller/SlidingViewController.m
+++ xsports/xsports/Source/Controller/SlidingViewController.m
@@ -7,15 +7,30 @@
 //
 
 #import "SlidingViewController.h"
+#import "MEZoomAnimationController.h"
+
+@interface SlidingViewController ()
+@property (strong, nonatomic) MEZoomAnimationController *zoomAnimator;
+@end
 
 @implementation SlidingViewController
 
 - (void)awakeFromNib
 {
     [super awakeFromNib];
+    self.zoomAnimator = [[MEZoomAnimationController alloc] init];
+    self.delegate = self.zoomAnimator;
+    self.topViewAnchoredGesture = ECSlidingViewControllerAnchoredGestureTapping | ECSlidingViewControllerAnchoredGesturePanning;
+
     self.menuViewController = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:MenuViewControllerIdentifier];
     self.underLeftViewController = self.menuViewController;
     [self.menuViewController select:kMenuItemNew animated:NO];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor lightPebbleColor];
 }
 
 @end
