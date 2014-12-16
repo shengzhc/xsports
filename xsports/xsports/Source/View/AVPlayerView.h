8efb174603ca47a878eacdc520adb69bb9b9e8b1
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-15 23:57:29 -0800

    update

---
+++ xsports/xsports/Source/View/AVPlayerView.h
@@ -0,0 +1,18 @@
+//
+//  AVPlayerView.h
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/15/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVPlayerView : UIView
+
+@property (nonatomic, strong) AVPlayer* player;
+
+- (void)setPlayer:(AVPlayer*)player;
+- (void)setVideoFillMode:(NSString *)fillMode;
+
+@end
---
+++ xsports/xsports/Source/View/AVPlayerView.m
@@ -0,0 +1,34 @@
+//
+//  AVPlayerView.m
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/15/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import "AVPlayerView.h"
+
+@implementation AVPlayerView
+
++ (Class)layerClass
+{
+    return [AVPlayerLayer class];
+}
+
+- (AVPlayer*)player
+{
+    return [(AVPlayerLayer*)[self layer] player];
+}
+
+- (void)setPlayer:(AVPlayer*)player
+{
+    [(AVPlayerLayer*)[self layer] setPlayer:player];
+}
+
+- (void)setVideoFillMode:(NSString *)fillMode
+{
+    AVPlayerLayer *playerLayer = (AVPlayerLayer*)[self layer];
+    playerLayer.videoGravity = fillMode;
+}
+
+@end
