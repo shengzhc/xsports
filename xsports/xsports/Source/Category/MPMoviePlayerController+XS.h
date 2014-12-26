2e2e6d2840eafb1130a88ad33b59944171dcfacc
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-25 21:37:57 -0800

    update

---
+++ xsports/xsports/Source/Category/MPMoviePlayerController+XS.h
@@ -0,0 +1,13 @@
+//
+//  MPMoviePlayerController+XS.h
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/25/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+@interface MPMoviePlayerController (XS)
+
++ (MPMoviePlayerController *)sharedMoviePlayerController;
+
+@end
