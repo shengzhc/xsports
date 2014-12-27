56ef334c7cf3e6b293d1d070d7034175f81bc2c4
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-27 06:35:24 -0800

    update

---
+++ xsports/xsports/Source/Category/AVPlayer+XS.h
@@ -0,0 +1,13 @@
+//
+//  AVPlayer+XS.h
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/27/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+@interface AVPlayer (XS)
+
+@end
