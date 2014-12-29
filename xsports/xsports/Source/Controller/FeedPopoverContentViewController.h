ce68002aaf14d21ab1e7fb524db1cbdc65cd9811
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-29 15:52:59 -0800

    set up groups

---
+++ xsports/xsports/Source/Controller/FeedPopoverContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedPopoverContentViewController.h
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/29/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedPopoverContentViewController : UITableViewController
+
+@end
