650c6c0ea89e2bfe6dfdca6356baa2326613581c
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-30 19:22:13 -0800

    update

---
+++ xsports/xsports/Source/Controller/CamCaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  CamCaptureViewController.h
+//  xsports
+//
+//  Created by Shengzhe Chen on 12/30/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CamCaptureViewController : UIViewController
+
+@end
--- xsports/xsports/XSConstants.h
+++ xsports/xsports/XSConstants.h
@@ -9,41 +9,56 @@
 #ifndef _XSConstants_
 #define _XSConstants_
 
+#pragma mark Login
 #define LoginViewControllerIdentifier @"LoginViewController"
 #define SignUpViewControllerIdentifier @"SignUpViewController"
-#define LoadingViewControllerIdentifier @"LoadingViewController"
-#define SlidingViewControllerIdentifier @"SlidingViewController"
-#define MenuViewControllerIdentifier @"MenuViewController"
-#define FeedViewControllerIdentifier @"FeedViewController"
-#define NavFeedViewControllerIdentifier @"Nav_FeedViewController"
-#define SettingViewControllerIdentifier @"SettingViewController"
-#define NavSettingViewControllerIdentifier @"Nav_SettingViewController"
-#define FeedFlowCollectionViewControllerIdentifier @"FeedFlowCollectionViewController"
-#define FeedGridCollectionViewControllerIdentifier @"FeedGridCollectionViewController"
-#define LikesViewControllerIdentifier @"LikesViewController"
-#define CommentsViewControllerIdentifier @"CommentsViewController"
-#define CommentsTextViewControllerIdentifier @"CommentsTextViewController"
-#define CamViewControllerIdentifier @"CamViewController"
-#define CamFilterViewControllerIdentifier @"CamFilterViewController"
-#define NavAVCamViewControllerIdentifier @"Nav_AVCamViewController"
-#define AssetsPickerViewControllerIdentifier @"AssetsPickerViewController"
-#define FeedPopoverContentViewControllerIdentifier @"FeedPopoverContentViewController"
 
 #define LoginEmailCellIdentifier @"LoginEmailCell"
 #define LoginPasswordCellIdentifier @"LoginPasswordCell"
 #define SignupAccountCellIdentifier @"SignupAccountCell"
-#define MenuViewCellIdentifier @"MenuViewCell"
+
+#define LoginSignUpViewControllerSegueIdentifier @"SignUpViewControllerSegue"
+
+
+#pragma mark Feed
+#define FeedViewControllerIdentifier @"FeedViewController"
+#define NavFeedViewControllerIdentifier @"Nav_FeedViewController"
+#define FeedFlowCollectionViewControllerIdentifier @"FeedFlowCollectionViewController"
+#define FeedGridCollectionViewControllerIdentifier @"FeedGridCollectionViewController"
+#define FeedPopoverContentViewControllerIdentifier @"FeedPopoverContentViewController"
+
 #define FeedViewPhotoCellIdentifier @"FeedViewPhotoCell"
 #define FeedViewVideoCellIdentifier @"FeedViewVideoCell"
 #define FeedViewGridPhotoCellIdentifier @"FeedViewGridPhotoCell"
 #define FeedViewGridVideoCellIdentifier @"FeedViewGridVideoCell"
-#define LikesCellIdentifier @"LikesCell"
-#define CommentsCellIdentifier @"CommentsCell"
 #define FeedPopoverContentCellIdentifier @"FeedPopoverContentCell"
 
 #define FeedViewFeedFlowLayoutSegueIdentifier @"FeedView_FeedFlowLayout_Segue"
-#define CommentsTextViewControllerSegueIdentifier @"Comments_TextViewControllerSegue"
+
+
+#pragma mark Cam
+#define CamViewControllerIdentifier @"CamViewController"
+#define CamFilterViewControllerIdentifier @"CamFilterViewController"
+#define NavAVCamViewControllerIdentifier @"Nav_AVCamViewController"
+#define AssetsPickerViewControllerIdentifier @"AssetsPickerViewController"
+
 #define CamFilterViewControllerSegueIdentifier @"CamFilterViewControllerSegue"
-#define LoginSignUpViewControllerSegueIdentifier @"SignUpViewControllerSegue"
+
+
+#pragma mark Other
+#define LoadingViewControllerIdentifier @"LoadingViewController"
+#define SlidingViewControllerIdentifier @"SlidingViewController"
+#define MenuViewControllerIdentifier @"MenuViewController"
+#define SettingViewControllerIdentifier @"SettingViewController"
+#define NavSettingViewControllerIdentifier @"Nav_SettingViewController"
+#define LikesViewControllerIdentifier @"LikesViewController"
+#define CommentsViewControllerIdentifier @"CommentsViewController"
+#define CommentsTextViewControllerIdentifier @"CommentsTextViewController"
+
+#define MenuViewCellIdentifier @"MenuViewCell"
+#define LikesCellIdentifier @"LikesCell"
+#define CommentsCellIdentifier @"CommentsCell"
+
+#define CommentsTextViewControllerSegueIdentifier @"Comments_TextViewControllerSegue"
 
 #endif
